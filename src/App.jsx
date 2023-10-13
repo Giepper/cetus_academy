c8c64fd5dce3f5288d58d4405e97df9f0132b604
Albert <niziolalbert@gmail.com>
2023-10-13 10:46:48 +0200

    wybor poziomow

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,6 @@ function App() {
           levelValue={levelValue}
         />
       )}
-      
     </>
   );
 }
--- src/components/Levels.jsx
+++ src/components/Levels.jsx
@@ -1,13 +1,19 @@
 import "./Levels.css";
 
 export function Levels(props) {
+  let previousLevel = 0;
+  const isWin = localStorage.getItem("isWin");
+  const newLevel = localStorage.getItem("possiblyNewLevel");
+  if (localStorage.getItem("actualLevel")) {
+    previousLevel = localStorage.getItem("actualLevel");
+  } else {
+    localStorage.setItem("actualLevel", 0);
+  }
   function checkLevel() {
-    const isWin = localStorage.getItem("isWin");
-    const previousLevel = localStorage.getItem("actualLevel");
-    const newLevel = localStorage.getItem("possiblyNewLevel");
     console.log("playerWin", isWin);
     console.log("acutalLevel", previousLevel);
     console.log("possiblyNewLevel", newLevel);
+    // for (let x = 0; x < previousLevel; x++) {}
   }
   checkLevel();
 
@@ -21,74 +27,28 @@ export function Levels(props) {
       localStorage.setItem("possiblyNewLevel", tmp);
     }
   };
+  const divs = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
   return (
     <>
       <div className="levels-wrapper">
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div
-            className="level lvl-1 level-unlocked"
-            value={0}
-            onClick={handlerSelectLevel}
-          >
-            1
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-2" value="1" onClick={handlerSelectLevel}>
-            2
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-3" value="2" onClick={handlerSelectLevel}>
-            3
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-4" value="3" onClick={handlerSelectLevel}>
-            4
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-5" value="4" onClick={handlerSelectLevel}>
-            5
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-6" value="5" onClick={handlerSelectLevel}>
-            6
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-7" value="6" onClick={handlerSelectLevel}>
-            7
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-8" value="7" onClick={handlerSelectLevel}>
-            8
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-9" value="8" onClick={handlerSelectLevel}>
-            9
-          </div>
-        </div>
-        <div className="level-container">
-          <div className="cat-box"></div>
-          <div className="level lvl-10" value="9" onClick={handlerSelectLevel}>
-            10
-          </div>
-        </div>
+        {divs.map((divContent, index) => (
+          <>
+            <div className="level-container">
+              <div className="cat-box"></div>
+              <div
+                key={index}
+                className={`level lvl-${index + 1} ${
+                  index <= newLevel ? "level-unlocked" : ""
+                }`}
+                value={index}
+                onClick={handlerSelectLevel}
+              >
+                {divContent}
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </>
   );
